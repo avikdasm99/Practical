e7640cf79ca50ff25702f4bcac80a9dfa6498ac4
avikdasm99 <110466059+avikdasm99@users.noreply.github.com>
2022-08-09 17:36:54 +0530

    Add files via upload

---
+++ Subtraction.java
@@ -0,0 +1,18 @@
+class Subtraction{
+
+public static void main(String[]args){
+
+System.out.println("The two number is");
+
+int a=8;  //initialize 1st local variable
+int b=2;  //initialize 2nd local variable
+
+System.out.println(a+" "+b);
+
+
+
+int sub=a-b; //logic
+
+System.out.println("the Subtraction is = "+sub);
+}
+}
