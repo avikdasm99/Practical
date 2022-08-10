713b12adb85d27d1cc9ca66e103d23625fd04af2
avikdasm99 <110466059+avikdasm99@users.noreply.github.com>
2022-08-10 15:30:05 +0530

    Add files via upload

---
+++ Rectangle.java
@@ -0,0 +1,15 @@
+class Rectangle{
+public static void main(String ...args){
+
+float length=5.2f;
+float breadth=5.7f;
+
+double area=length*breadth  ;    //logic
+double perimeter=2*(length+breadth);   //logic
+
+
+System.out.println("the area of the Rectangle="+area);
+System.out.println("the perimeter of the Rectangle="+perimeter);
+
+}
+}
