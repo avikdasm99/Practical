3bc9eccb8d42803c1dc4432054a31884b2f61e54
avikdasm99 <110466059+avikdasm99@users.noreply.github.com>
2022-08-10 15:29:46 +0530

    Add files via upload

---
+++ Square.java
@@ -0,0 +1,14 @@
+class Square{
+public static void main(String ...args){
+
+float side=5.2f;
+
+float area= side*side;    //logic
+float perimeter=4*(side);   //logic
+
+
+System.out.println("the area of the square="+area);
+System.out.println("the perimeter of the square="+perimeter);
+
+}
+}
