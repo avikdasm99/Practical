456ccd0fb1c3b04018d3ae0eb280a0937b6f72ce
avikdasm99 <110466059+avikdasm99@users.noreply.github.com>
2022-08-17 15:03:59 +0530

    Add files via upload

---
+++ OperatorEx.java
@@ -0,0 +1,18 @@
+package Operator;
+
+public class OperatorEx {
+
+	public static void main(String[] args) {
+		
+		int a=10;
+		int b=20;
+		System.out.println(a+b);//addition
+		System.out.println(a-b);//subtraction
+		System.out.println(a*b);//multiplication
+		System.out.println(a/b);//divition
+		System.out.println(a%b);//mod
+		System.out.println(a>b||a<b);//OR
+		System.out.println(a>b&&a<b);//AND
+	}
+
+}
