dc8b974e4290bfe8b5af71f36b0c95078bb83a9e
avikdasm99 <110466059+avikdasm99@users.noreply.github.com>
2022-08-18 09:23:35 +0530

    Add files via upload

---
+++ Prefix_Postfix.java
@@ -0,0 +1,20 @@
+package Operator;
+ 
+
+public class Prefix_Postfix {
+
+	public static void main(String[] args) {
+		
+		int a=3; //initialize variable
+		
+		int b=a++; //Prefix
+		
+		int c=++b;  //Postfix
+		
+		int d= a+b+c; //logic
+		
+		System.out.println("The result is = "+d);  //print
+
+	}
+
+}
