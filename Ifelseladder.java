c35099c7f15859a46bc106b20c8cf43e222a53c7
avikdasm99 <110466059+avikdasm99@users.noreply.github.com>
2022-08-19 16:42:08 +0530

    Add files via upload

---
+++ Ifelseladder.java
@@ -0,0 +1,22 @@
+
+public class Ifelseladder {
+
+	public static void main(String[] args) {
+		
+		String name="AVIK";
+		if(name=="meena") {
+			System.out.println("The name is meena");
+		}
+		else if(name=="neha") {
+			System.out.println("The name is meena");
+		}
+		else if(name=="amit") {
+			System.out.println("The name is amit");
+		}
+		else {
+			System.out.println(name);
+		}
+
+	}
+
+}
