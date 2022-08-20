61c8b6e4aed3d175b15c9ae588c5e7a1248c762f
avikdasm99 <110466059+avikdasm99@users.noreply.github.com>
2022-08-20 09:33:52 +0530

    Add files via upload

---
+++ Nestedif.java
@@ -0,0 +1,29 @@
+
+public class Nestedif {
+
+	public static void main(String[] args) {
+		
+		String address="Kolkata,India";
+		
+		if(address.endsWith("India")){
+			
+			if(address.contains("Dunlop")) {
+				System.out.println("Your city is Dunlop");
+		}
+			
+		else if(address.contains("Newtown")) {
+			System.out.println("Your city is Newtown");
+		}
+			
+		else{
+			System.out.println(address.split(",")[0]);
+		}
+			
+		}
+		else{
+			System.out.println("You are not living in India");
+		}
+
+	}
+
+}
