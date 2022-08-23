617c906bca31b761cc16d82e95ea1daf1b062eb1
avikdasm99 <110466059+avikdasm99@users.noreply.github.com>
2022-08-23 15:54:24 +0530

    Add files via upload

---
+++ SwitchTest.java
@@ -0,0 +1,21 @@
+
+public class SwitchTest {
+
+	public static void main(String[] args) {
+		int n=10;
+		switch(n) {
+		case 1:System.out.println("12");
+		break; 
+		
+	    case 6: System.out.println("12");
+	   break; 
+	   
+	   case 10:System.out.println("Very Good");
+		break; 
+		
+		default:System.out.println("the number is not within the range");
+		}
+	}
+}
+
+
