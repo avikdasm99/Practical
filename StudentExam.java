fa666df8623aaf56223d8974f526a9d0ddd7a309
avikdasm99 <110466059+avikdasm99@users.noreply.github.com>
2022-08-30 14:11:40 +0530

    Add files via upload

---
+++ StudentExam.java
@@ -0,0 +1,14 @@
+package OOP_Concept;
+
+class StudentExam1 {
+	int age;
+	String name;
+}
+
+
+public class StudentExam{
+	public static void main(String ...args) {
+		StudentExam1 obj=new StudentExam1();
+		System.out.println(obj.age+"  ___   "+obj.name);
+	}
+}
