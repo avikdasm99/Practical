80a7b3c5e2c6b81936d130f43574be5b9cf6b844
avikdasm99 <110466059+avikdasm99@users.noreply.github.com>
2022-08-30 17:35:04 +0530

    Add files via upload

---
+++ Default_Const.java
@@ -0,0 +1,17 @@
+package OOP_Concept;
+
+public class Default_Const {
+
+	Default_Const(){
+		System.out.println(" default constructor created");	
+}
+
+	public static void main(String[] args) {
+		
+		Default_Const d=new Default_Const();
+		
+	
+
+	}
+
+}
