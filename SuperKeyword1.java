61129bb281896735978165ac7a213fed2d413044
avikdasm99 <110466059+avikdasm99@users.noreply.github.com>
2022-09-14 17:01:40 +0530

    Add files via upload

---
+++ SuperKeyword1.java
@@ -0,0 +1,19 @@
+package practical;
+
+class Animal{  
+  String color="white";  
+}  
+
+class Dog extends Animal{  
+	String color="black";  
+	void printColor(){  
+		System.out.println(color);
+        System.out.println(super.color);
+  }  
+}  
+public class SuperKeyword1{  
+public static void main(String args[]){  
+Dog d=new Dog();  
+d.printColor();  
+   }
+}    
