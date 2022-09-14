2eaab1561877a127125849109ca0f07e94b91d77
avikdasm99 <110466059+avikdasm99@users.noreply.github.com>
2022-09-14 17:36:26 +0530

    Add files via upload

---
+++ SuperKeyWord3.java
@@ -0,0 +1,22 @@
+package practical;
+
+class Animal{ 
+	
+   Animal(){
+	System.out.println("animal is created");
+         }  
+   }  
+
+class Dog extends Animal{  
+Dog(){  
+super(); 
+System.out.println("dog is created");  
+   }  
+} 
+
+public class SuperKeyWord3{  
+   public static void main(String args[]){  
+       Dog d=new Dog();
+      
+   }
+}  
