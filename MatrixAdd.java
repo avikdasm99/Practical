0a1a8a7678fe282e3471f2acfe42f24081a13641
avikdasm99 <110466059+avikdasm99@users.noreply.github.com>
2022-09-29 15:54:35 +0530

    Add files via upload

---
+++ MatrixAdd.java
@@ -0,0 +1,19 @@
+package rough;
+
+public class MatrixAdd {
+
+	public static void main(String[] args) {
+		int a[][]= { {1,2},{3,4} };
+		int b[][]= { {1,2},{3,4} };
+		int c[][]=new int[8][3];    
+		
+	
+		for(int i=0;i<2;i++) {
+			for(int j=0;j<2;j++) {
+				c[i][j]=a[i][j]+b[i][j];
+				System.out.print(c[i][j]+"    ");
+			}
+			System.out.println();
+		}
+	}
+}
