c85edee1584060c776098e380d507fa2b186ef35
avikdasm99 <110466059+avikdasm99@users.noreply.github.com>
2022-09-29 15:06:26 +0530

    Add files via upload

---
+++ Classname.java
@@ -0,0 +1,15 @@
+package arr;
+public class Classname{  
+public static void main(String args[]){  
+
+int arr[]={4,4,5};  
+
+Class c=arr.getClass();  
+
+String name=c.getName();  
+
+System.out.println(name);  
+System.out.println(c);
+
+      }
+}  
